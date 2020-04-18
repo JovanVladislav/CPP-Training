1232fa1061e3876896aaf4d435f94e878b83897b
Jovan Vladislav <JovanVladislav@protonmail.com>
2020-04-18 11:19:52 -0700

    First Program

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int day;
+    cout << "Enter a number : 1 - 5 \n";
+    cin >> day;
+    switch (day) {
+        case 1:
+            if(day == 1){
+                cout <<"Hello Noob C++ Programmer";
+                break;
+             case 2:
+                 if(day == 2){
+                     cout << "Hello C++ programming padavan";
+                     break;
+                  case 3:
+                  if(day == 3){
+                      cout << "Hello C++ Master CODER";
+                      break;
+
+                  case 4:
+                      if(day == 4){
+                          cout << "Hello, GOD OF C++";
+                          break;
+
+                          case 5:
+                              if(day == 5){
+                                  cout << "UR THE LIVING GOD OF C++";
+                                  break;
+
+                                  default: cout << "Oops, someting went wrong in compiling";
+                              }
+                      }
+
+                  }
+
+                 }
+            }
+
+    }
+
+    return 0;
+
+}
